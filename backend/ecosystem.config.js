444fec7a961b9316fe2a037f02f3a95d4188ecd3
Yulia Berezhnaya <p.kotta@yandex.ru>
2023-12-06 23:27:21 +0300

    Добавлен файл для деплоя бэкенда

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,24 @@
+require('dotenv').config({ path: '.env.deploy' });
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REPOSITORY, DEPLOY_REF,
+} = process.env;
+
+module.exports = {
+  apps: [{
+    name: 'api-mesto',
+    script: './dist/app.js',
+  }],
+
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPOSITORY,
+      path: DEPLOY_PATH,
+      'pre-deploy-local': `bash scripts/deployEnv.sh ${DEPLOY_USER}@${DEPLOY_HOST} ${DEPLOY_PATH}`,
+      'post-deploy': 'pwd && npm ci && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
+    },
+  },
+};
