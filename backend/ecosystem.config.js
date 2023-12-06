ce7abf39440d23f7faab9b27a2eef62ccb927b47
Yulia Berezhnaya <p.kotta@yandex.ru>
2023-12-06 23:37:55 +0300

    Фикс post-deploy директивы

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -18,7 +18,7 @@ module.exports = {
       repo: DEPLOY_REPOSITORY,
       path: DEPLOY_PATH,
       'pre-deploy-local': `bash scripts/deployEnv.sh ${DEPLOY_USER}@${DEPLOY_HOST} ${DEPLOY_PATH}`,
-      'post-deploy': 'pwd && npm i && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
+      'post-deploy': 'cd backend && pwd && npm i && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
     },
   },
 };
